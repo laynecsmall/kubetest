165dcfe224c1ee996d7f5f119ced8a3580105fb3
Layne Small <laynecsmall@gmail.com>
2019-04-11 12:29:05 +1200

    initial

---
+++ express-test/test.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const fetch = require('node-fetch')
+const app = express()
+const port = 3000
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.post('/', function (req, res) {
+	const r = { bd: req.body, method: 'http'};
+	fetch('http://logstash:5000', {method: 'POST', body: JSON.stringify(r)});
+	res.send('Hello World!');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
